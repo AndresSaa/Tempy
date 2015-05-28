00eebc827ccf70990737f5db416febda397c8608
AndresSaa <andres.saa.narvaez@gmail.com>
2015-05-28 18:20:37 +0200

    Support AMD & CommonJS

--- tempy.js
+++ tempy.js
@@ -1,11 +1,23 @@
-(function(){
+(function (factory) {
+    
+    if (typeof define === 'function' && define.amd)
+        // AMD (Register as an anonymous module)
+        define( factory );
+    else if (typeof exports === 'object')
+        // Node/CommonJS
+        module.exports = factory();
+    else
+        // Browser globals
+        window.Tempy =  factory();
 
-	var tempy = function ( html, data ) {
+}(function(){
+
+	var Tempy = function ( html, data ) {
 		for ( var value in data )
 			html = html.replace( new RegExp( '{'+value+'}', 'g' ), data[value] );
 		return html;
 	};
 	
-	this.tempy = tempy;
+	return Tempy;
 
-}).call(this);
+}));
